eb7e29055a44bda90847792913fe1b897597be2c
Dave S <32913068+stone4789@users.noreply.github.com>
2020-02-06 15:47:37 -0700

    Went back to dict version of graph
    find_route now works:
    Input: find_route(graph, 'berlin','saarbruecken')
    Output: ['berlin', 'magdeburg', 'hannover', 'kassel', 'frankfurt', 'saarbruecken', 841]

--- project1/src/ucs_redo.py
+++ project1/src/ucs_redo.py
@@ -1,6 +1,6 @@
 # It's getting caught trying to get edges with edges=graph[node], so I'm trying to slice instead for now.
 import sys
-from PriorityQueue import queue
+from queue import PriorityQueue
 
 def main():
     # sys.argv[1] = input_filename
@@ -24,57 +24,39 @@ def main():
     path = find_route(graph, source, dest)
 
 # handles placing the input file into an appropriate data structure
-def parse_input(input_file):
-
-    f = open(input_file, "r")
-    graph = []
-    for line in f:
-        if "END OF INPUT" in line:
-            break
-        else:
-            cities = line.split()
-
-            sources = cities[0]
-            destinations = cities[1]
-            weight = cities[2]
-
-            # unidirectional graph
-            orig_path = [sources, destinations, weight]
-            rev_path = [destinations, sources, weight]
-
-            graph.append(orig_path)
-            graph.append(rev_path)
-            columns = list(zip(*graph))
-    return graph
+def parse_input(file_name):
+    graph = {}
+    file = open(file_name, 'r')
+    for line in file:
+        if 'END OF INPUT' in line:
+            return graph
+        node1, node2, distance = line.lower().split()
+        graph.setdefault(node1, []).append((node2, distance))
+        graph.setdefault(node2, []).append((node1, distance))
+        
+graph = construct_graph(filepath)
+path = []
 
 def find_route(graph, source, destination):
-    fringe = set()
-    q = queue()
+    frontier = set()
+    q = PriorityQueue()
     q.put((0, [source]))
-    #
-    source = [x for x in columns[0] if x == "{}".format(source)]
-    destination = [x for x in columns[1] if x == "{}".format(destination)]
-    weights = [x for x in columns[2] if columns[0] == source and columns[1] == destination]
-    #
-    while not q.empty():
-        weight, route = q.get()
+    while q.empty() is False:
+        cost, route = q.get()
         node = route[len(route) -1]
-
-        if node not in fringe:
-            fringe.add(node)
+        if node not in frontier:
+            frontier.add(node)
             if node == destination:
-                route.append(weight)
+                route.append(cost)
                 return route
-
-            # graph is a 2D list and not a dict
             edges = graph[node]
-            for edge in [edges[0] for edge in edges]:
-                if edge not in fringe:
-                    index = [node[0] for node in graph[node]].index(edge)
-                    total_cost = weight + int(graph[node][index][1])
-                    visited = route[:]
-                    visited.append(edge)
-                    q.put((total_cost, visited))
+            for edge in [edge[0] for edge in edges]:
+                if edge not in frontier:
+                    loc = [n[0] for n in graph[node]].index(edge)
+                    path_cost = cost + int(graph[node][loc][1])
+                    last_route = route[:]
+                    last_route.append(edge)
+                    q.put((path_cost, last_route))
 
 def results(graph, visited):
     weights = visited[-1]
