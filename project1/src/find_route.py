8018b9d41e0864f0030e890635cf4ea4ddd6e87c
ncarn2 <carnivalnicholas2@gmail.com>
2020-02-06 20:31:06 -0700

    finished handling printing and running from the command line

--- project1/src/find_route.py
+++ project1/src/find_route.py
@@ -1,12 +1,7 @@
 import sys
-import math 
-from PriorityQueue import PriorityQueue2
+from queue import PriorityQueue
 
 def main():
-    """
-    Takes in three command line arguments in the form:
-    'find_route input_filename origin_city destination_city'
-    """
     # sys.argv[1] = input_filename
     # sys.argv[2] = origin_city
     # sys.argv[3] = destination_city
@@ -14,127 +9,80 @@ def main():
     if (len(sys.argv) != 4):
         print("Not enough arguments")
         sys.exit()
+
     file_name = sys.argv[1]
 
-    # Generate all of the cities
-    cities = parse_input(file_name)
-
     # Find the specified route
-    source = sys.argv[2]
-    dest = sys.argv[3]
-    route = uninformed_search(source, dest, cities)
-    print(route)
+    source = str(sys.argv[2]).upper()
+    destination = str(sys.argv[3]).upper()
 
-def uninformed_search(source, destination, cities):
-    """
-    This algorithm takes in the source, destination, and a dictionary of cities and adjacencies basically a smallest cost first algorithm
-    """
-    #[ {'Bremen' : 14}, {'Frankfurt' : 43} ]
-    current_node = source
-    print('Original Current Node: ', current_node)
+    # Generate all of the cities
+    graph = parse_input(file_name)
+    path = []
 
-    done = False
-    count = 0
-    smallest_weight = math.inf  
+    # TODO: this line causes error right now:
+    results = find_route(graph, source, destination)
+    print_results(graph, results)
 
-    visited = set()
+def print_results(graph, results):
+    if results is not None:
+        print("distance: ", results[-1],"km")
 
-    frontier = PriorityQueue2()
-    # create the graph
+        print_statement = ''
 
-    frontier.put((0, [current_node])
-    while frontier.empty() is False:
-        print("Current Node:", current_node, "\n")
-        print(frontier)
-        weight, route = frontier.get()
-        if node not in visited:
-            visited.add(node)
+        previous_node = 'EMPTY'
+
+        for current_node in results[:-1]:
+
+            if previous_node != 'EMPTY':
+                for connection in graph[current_node]:
+                    if connection[0] == previous_node:
+                        print(previous_node, "to ", current_node, ",", connection[1], "km")
+
+            previous_node = current_node
+    else:
+        print("distance: infinity")
+        print("route:")
+        print("none")
+
+# handles placing the input file into an appropriate data structure
+def parse_input(file_name):
+    graph = {}
+
+    file = open(file_name, 'r')
+
+    for line in file:
+        if 'END OF INPUT' in line:
+            return graph
+        node1, node2, distance = line.lower().split()
+
+        node1 = node1.upper()
+        node2 = node2.upper()
+
+        graph.setdefault(node1, []).append((node2, distance))
+        graph.setdefault(node2, []).append((node1, distance))
+
+def find_route(graph, source, destination):
+    frontier = set()
+    q = PriorityQueue()
+    q.put((0, [source]))
+    while q.empty() is False:
+        cost, route = q.get()
+        node = route[len(route) -1]
+        if node not in frontier:
+            frontier.add(node)
             if node == destination:
-                route.append(weight)
+                route.append(cost)
                 return route
             edges = graph[node]
             for edge in [edge[0] for edge in edges]:
-                if edge not in visited:
-                    current_node = [n[0] for n in graph[node]].index(edge)
-                    cost = weight + int(graph[node][current_node][1])
-                    current_route = route[:]
-                    current_route.append(edge)
-                    queue.put((cost, current_route))
-
-
-        #---------------------------------------------------------------
-        visited.add(current_node)
-        count += 1
-        if count > 7:
-            break
-        parent = frontier.get()
-
-        # set the parent node for all of the children
-        parent_weight = parent[0]
-        parent_name = parent[1]
-        parent = str(parent_name) + str(parent_weight)
-        parent_node = {'Parent' : parent}
-        cities[current_node].append(parent_node)
-        
-        # at each child node, we want to remove the parent node name and set the weight equal to the child plus the parent weights
-        # find the lowest weight
-        children_nodes = cities[current_node]
-        for child in children_nodes:
-            current_name = str(list(child.keys())[0])
-            if current_name != 'Parent':
-                current_weight = int(list(child.values())[0])
-
-                # add each of the adj nodes to the PriorityQueue
-                if current_name not in visited:
-                    frontier.put(current_weight, current_name)
-            # if it is a parent node, do something...
-            elif current_name == 'Parent':
-                pass
-        cheapest_node = frontier.get()
-        print("Cheapest Child:", cheapest_node, "\n")
-        current_node = cheapest_node[1]
-    return '\'route\'' 
-
-def parse_input(file_name):
-    """
-    Parses the input and creates an array of cities and their adjacencies
-    """
-    cities = {}
-    with open(file_name, 'r') as f:
-        line = f.readline()
-         
-        cities = {}
-        while line:
-            # Leave the loop at the end of the file
-            if line == 'END OF INPUT\n':
-                break
-            city, adjacency, weight = line.strip().split(' ') 
-
-            adj_weight = {adjacency : weight}
-            city_weight = {city : weight}
-
-            # if the city is in cities
-            if cities.get(city, 'f') != 'f':
-                # add adjacency
-                cities[city].append(adj_weight)
-                if cities.get(adjacency, 'f') == 'f':
-                    # if the adjacent city is not in the dict
-                    cities[adjacency] = [city_weight]
-                else:
-                    # if the adjacent city is in the dict
-                    cities[adjacency].append(city_weight)
-            # if the city is not in cities
-            elif cities.get(city, 'f') == 'f':
-                # add new city
-                cities[city] = [adj_weight]
-                if cities.get(adjacency, 'f') == 'f':
-                    # if the adjacent city is not in the dict
-                    cities[adjacency] = [city_weight]
-                else:
-                    # if the adjacent city is in the dict
-                    cities[adjacency].append(city_weight)
-
-            line = f.readline()
-    return cities
+                if edge not in frontier:
+                    loc = [n[0] for n in graph[node]].index(edge)
+                    path_cost = cost + int(graph[node][loc][1])
+                    last_route = route[:]
+                    last_route.append(edge)
+                    q.put((path_cost, last_route))
+                    
+                    
 if __name__ == "__main__":
     main()
--- project1/src/run.sh
+++ project1/src/run.sh
@@ -1,4 +1,4 @@
-#echo '[ ] Running python find_route.py Bremen Frankfurt'
-echo '[ ] Running python ucs_redo.py Bremen Frankfurt'
-#python3 ./find_route.py ../inputs/input.txt Bremen Frankfurt
-python3 ./ucs_redo.py ../inputs/input.txt Bremen Frankfurt
+echo '[ ] Running python find_route.py Bremen Frankfurt'
+python3 ./find_route.py ../inputs/input.txt Bremen Frankfurt
+echo '[ ] Running python find_route.py London Frankfurt'
+python3 ./find_route.py ../inputs/input.txt Bremen Frankfurt
